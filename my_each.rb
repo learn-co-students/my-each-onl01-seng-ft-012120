db21ded08edf924c1cd2bffffd45049e32b095e7
Roberto Livi <robertomlivi@outlook.com>
2020-01-25 05:08:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if collection.length == 0
+    "There is nothing in this collection"
+  end
+  i = 0
+  while i < collection.length
+  yield(collection[i])
+  i += 1
+  end
+  collection
 end
+
+my_each(["arel", "john", "logan", "spencer"]){|u| "#{u}"}
